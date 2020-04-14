a3ef34258e2744d90264fd8d097f14174869524b
Jose Manuel Contardo <jmcontardo@gmail.com>
2020-04-13 20:16:27 -0400

    cambio2

---
+++ mandelbrot.py
@@ -0,0 +1,28 @@
+import numpy as np
+import matplotlib.pyplot as plt # todo esto
+
+def mandelbrot ( h, w, maxit=20):
+    """Image of mandelbrot"""
+
+    y,x = np.ogrid[-1.4:1.4:h*1j, -2:0.8:w*1j]
+    c=x+y*1j
+    z = c
+    divtime = maxit + np.zeros(z.shape, dtype=int)
+
+    for i in range(maxit):
+        z = z**2 + c
+        diverge = z*np.conj(z)> 2**2
+        div_now = diverge & (divtime==maxit)
+        divtime[div_now] = i
+        z[diverge]=2
+
+    return divtime
+
+#md = mandelbrot(40,40)
+#print(md)
+#plt.imshow(md)
+#plt.show()
+
+for i in range(4,400):
+    plt.imshow(mandelbrot(i,i))
+    plt.show()
